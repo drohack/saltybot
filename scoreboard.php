012a4f2227287a78a21d92ab8905623918b06d93
drohack <drohack@gmail.com>
2015-09-09 17:07:42 -0500

    Update scoreboard
    Scoreboard now displays bet side, and orders by win rate.

--- scoreboard.php
+++ scoreboard.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="style.css" type="text/css">
+
 <?php 
 //database server 
 define('db_server', 'localhost'); 
@@ -19,16 +21,16 @@ if (!$db) {
 
 	//run query and output results 
 	//run a select query 
-	$select_query = 'SELECT uniqueId, username, saltyBucks, betAmount, odds, winRate FROM users'; 
+	$select_query = 'SELECT username, saltyBucks, betAmount, betSide, odds, winRate FROM users ORDER BY winRate'; 
 	$result = mysql_query($select_query); 
 
 	//output data in a table 
 	echo "<table border='1' style='width:100%;border: 1px solid black;border-collapse: collapse;padding: 5px;'\n"; 
 	echo "<tr>\n";
-	echo "<th>Unique ID</th>\n";
 	echo "<th>Username</th>\n";
 	echo "<th>Salty Bucks</th>\n";
 	echo "<th>Bet Amount</th>\n";
+	echo "<th>Bet Side</th>\n";
 	echo "<th>Odds</th>\n";
 	echo "<th>Win Rate</th>\n";
 	while ($row = mysql_fetch_row($result)){     
