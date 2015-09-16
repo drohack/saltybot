4234cf8289a0a633b21b3fc6a7b9e3490c52fe00
drohack <drohack@gmail.com>
2015-09-15 19:56:51 -0500

    Video page shows current fighter info and centered video.
    Easy to implement, might not be relevant if video is always full screen.

--- video.php
+++ video.php
@@ -1,4 +1,9 @@
 <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js'></script>
+<?php
+ob_start();
+include_once("functions/loadCurrentVideo.php");
+ob_end_clean();
+?>
 
 <script type='text/javascript'>
 	function playCurrentVideo() {
@@ -38,11 +43,47 @@
 	}
 </script>
 
-<video id="myVideo" height="80%" onended="videoEnded()" controls>
+<table id="currentFighterInfo" border='1' style='width:100%;border: 1px solid black;border-collapse: collapse;padding: 5px;'>
+	<tr>
+		<th colspan="4">CURRENT/NEXT FIGHT</th>
+	</tr>
+	<tr>
+		<th colspan="2">Red / Odds</th>
+		<th colspan="2">Blue / Odds</th>
+	</tr>
+	<tr>
+		<td width="25%" align="center"><?php echo $current_red_fighter; ?></td>
+		<td width="25%" align="center"><?php echo $current_red_odds; ?></td>
+		<td width="25%" align="center"><?php echo $current_blue_fighter; ?></td>
+		<td width="25%" align="center"><?php echo $current_blue_odds; ?></td>
+	</tr>
+</table>
+
+</br>
+
+<video id="myVideo" height="80%" onended="videoEnded()" style="margin:0 auto; width:100%;" controls>
 	<source src="videos/s1.mp4" type="video/mp4">
 	Your browser does not support HTML5 video.
 </video>
 
 </br></br>
 
-<button onclick="playCurrentVideo()">Play "current" video</button>
+<div align="center">
+	<button onclick="playCurrentVideo()">Play "current" video</button>
+</div>
+
+<script text="text/javascript">
+	setInterval(function(){
+		$.ajax({
+			type: 'GET',
+			url: 'functions/loadCurrentVideo.php',
+			success: refreshFighterInfo,
+			error: drawError
+		 });
+	}, 1000);
+	
+	function refreshFighterInfo() {
+		jQuery('#currentFighterInfo').load(document.URL +  ' #currentFighterInfo');
+		return false;
+	}
+</script>
