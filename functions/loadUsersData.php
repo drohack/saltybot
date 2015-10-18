aecdb4735e8f7ee552fb20f1ec4c73a4a68521a9
drohack <drohack@gmail.com>
2015-10-17 23:05:40 -0500

    Added arrows on scoreboard page to denote if people bet correctly last round or not.

--- functions/loadUsersData.php
+++ functions/loadUsersData.php
@@ -13,14 +13,15 @@ try {
 
 	//run query and output results 
 	//run a select query 
-	$users_query = $conn->prepare('SELECT username, saltyBucks, betAmount, betSide, odds, wins, gamesPlayed FROM users ORDER BY (wins/IF(gamesPlayed = 0, 1, gamesPlayed)) desc');
+	$users_query = $conn->prepare('SELECT id, username, saltyBucks, betAmount, betSide, odds, wins, gamesPlayed FROM users ORDER BY (wins/IF(gamesPlayed = 0, 1, gamesPlayed)) desc');
 	$users_query->execute();
 	
-	$red_fighter_query = $conn->prepare('SELECT red_fighter FROM current_video WHERE id=1;');
+	$red_fighter_query = $conn->prepare('SELECT video_id, red_fighter FROM current_video WHERE id=1;');
 	$red_fighter_query->execute();
 	$red_fighter_row = $red_fighter_query->fetch();
 	if($red_fighter_row) {
 		$this_red_fighter = $red_fighter_row['red_fighter'];
+		$last_video_id = $red_fighter_row['video_id'];
 	}
 
 	//output data in a table 
@@ -33,6 +34,37 @@ try {
 	echo "<th>Odds</th>\n";
 	echo "<th>Win Rate</th>\n";
 	while ($row = $users_query->fetch()){
+		//Get last winner
+		$last_winner = null;
+		$last_winner_query = $conn->prepare('SELECT winner FROM videos WHERE id=' . ($last_video_id - 1) . ';');
+		$last_winner_query->execute();
+		$last_winner_row = $last_winner_query->fetch();
+		if($last_winner_row) {
+			$last_winner = $last_winner_row['winner'];
+		}
+		
+		//Get user's last bet 
+		$last_bet_side = null;
+		$last_bet_side_query = $conn->prepare('SELECT betSide FROM user_logs WHERE user_id=' . $row['id'] . ' AND video_id=' . ($last_video_id - 1) . ';');
+		$last_bet_side_query->execute();
+		$last_bet_side_row = $last_bet_side_query->fetch();
+		if($last_bet_side_row) {
+			$last_bet_side = $last_bet_side_row['betSide'];
+		}
+		
+		//Check to see if there was previously a fight. And if the user previously bet on that fight.
+		$previously_bet = 0;
+		if($last_winner != null && $last_winner != "" && $last_bet_side != null && $last_bet_side != "") {
+			//If the user bet correctly last fight then add a big green plus next to their name
+			//Else add a big red minus next to their name
+			$previously_bet = 1;
+			if($last_winner == $last_bet_side) {
+				$previously_won = 1;
+			} else {
+				$previously_lost = 0;
+			}
+		}
+		
 		$winRate = "";
 		if($row['wins'] != "" && $row['gamesPlayed'] != "") {
 			$winRate = $row['wins'] / $row['gamesPlayed'] * 100;
@@ -49,7 +81,17 @@ try {
 		}
 		echo "<tr>\n"; 
 			echo "<td>" . $row['username'] . "</td>\n"; 
-			echo "<td>" . $row['saltyBucks'] . "</td>\n"; 
+			echo "<td>";
+			if($previously_bet == 1) {
+				if($previously_won == 1) {
+					echo '<div width="100%">' . $row['saltyBucks'] . '<strong><font color="green" style="padding-right: 10px;float:right;-webkit-transform: scale(2,1);-moz-transform: scale(2,1);-ms-transform: scale(2,1);transform: scale(2,1);">&uarr;</font></strong></div>';
+				} else {
+					echo '<div width="100%">' . $row['saltyBucks'] . '<strong><font color="red" style="padding-right: 10px;float:right;-webkit-transform: scale(2,1);-moz-transform: scale(2,1);-ms-transform: scale(2,1);transform: scale(2,1);">&darr;</font></strong><div>';
+				}
+			} else {
+				echo $row['saltyBucks'];
+			}
+			echo "</td>\n";
 			echo "<td>" . $row['betAmount'] . "</td>\n"; 
 			echo "<td>" . $formatted_bet_side . "</td>\n"; 
 			echo "<td>" . $formatted_odds . "</td>\n"; 
